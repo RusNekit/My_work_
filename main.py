a6ff432436b9764bf2ad88810ec3e546bb72fcfa
Nikita <rusnekit@gmail.com>
2024-10-22 22:49:38 +0500

    22/10/2024/ vev2-1-1

--- main.py
+++ main.py
@@ -4,13 +4,4 @@ print((9**0.5)*5)
 # "2nd program"
 print(bool(9.99 > 9.98 and 1000 != 1000.1))
 
-# "3rd program"
-a = (2 * 2 + 2)
-print(a)
-b = 2 * (2 + 2)
-print(b)
-print(bool(a == b))
 
-# "4th program"
-s = '123.456'
-print(int(float(s) * 10) % 10)
