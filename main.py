637a11714d60dc4ee887b0c0c2f479190b075beb
Nikita <rusnekit@gmail.com>
2024-10-22 22:36:51 +0500

    22/10/2024/ vev2

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# "1st program"
+print((9**0.5)*5)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# "2nd program"
+print(bool(9.99 > 9.98 and 1000 != 1000.1))
 
+# "3rd program"
+a = (2 * 2 + 2)
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(bool(a == b))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# "4th program"
+s = '123.456'
+print(int(float(s) * 10) % 10)
