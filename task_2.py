469cbd291a380f511d6e84c3eb46531885f01510
Nikita <rusnekit@gmail.com>
2024-10-23 20:49:23 +0500

    23/10/2024 Mod1_task_2

---
+++ task_2.py
@@ -0,0 +1,9 @@
+completed_tasks = 12
+time_spent = 1.5
+course_name = "Python."
+average_execution_time = time_spent / completed_tasks
+completed_tasks = '12.'
+time_spent = '1.5.'
+print('Курс:', course_name, 'Всего задач:', completed_tasks,
+      'Затрачено часов:', time_spent,
+      'Среднее время выполнения:', average_execution_time)
