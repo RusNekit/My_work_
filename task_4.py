e060047bf53b153b60ed8867f782f97d05f330d4
RusNekit <rusnekit@gmail.com>
2024-10-24 22:28:34 +0500

    23/10/2024 Mod1_task_4

---
+++ task_4.py
@@ -0,0 +1,17 @@
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+#/
+# name = input('Как тебя звать бро?: ')
+# current_year = 2024
+# your_birth = int(input('В каком году ты родился?: '))
+# age = current_year - your_birth
+# print('Hi', name)
+# print('It is in year', age, 'year')
