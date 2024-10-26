254ebd3ad2bd9caaca4003069c7d03920847a7d6
RusNekit <rusnekit@gmail.com>
2024-10-26 10:52:28 +0500

    23/10/2024 Mod1_task_5

--- task_5.py
+++ task_5.py
@@ -1,12 +1,3 @@
-# food = ['appple', 'coconat', 'banana']
-# print(food[2])
-# food.append('big')
-# food.extend('good')
-# print(food[1:11:2])
-# food.remove('coconat')
-# print(len(food))
-from math import trunc
-
 immutable_var = (4, 'good boy', True)
 print(immutable_var)
 
