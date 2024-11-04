095508636730fd344ac2aa35097aad3a6e34aed9
RusNekit <rusnekit@gmail.com>
2024-11-04 21:09:23 +0500

    04/11/2024 tast_01

--- task_01.py
+++ task_01.py
@@ -1,5 +1,21 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+calls =0
+def count_calls():
+    global calls
+    sum(calls)
+    print(calls)
+
+
+
+
+def string_info(a):
+    global calls
+    calls += 1
+    a = len(a), a.upper(), a.lower()
+    print(a)
+
+
+#def is_contains(s, d):
+
+
+string_info('NikiTa')
+string_info('cOpibara')
