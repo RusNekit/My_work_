9d875288d8ffe08225b42f18fb62fa6e830454bd
RusNekit <rusnekit@gmail.com>
2024-11-05 21:43:53 +0500

    05/11/2024 tast_01

--- task_01.py
+++ task_01.py
@@ -1,21 +1,28 @@
-calls =0
+calls = 0
+
 def count_calls():
     global calls
-    sum(calls)
-    print(calls)
-
-
-
+    calls += 1
 
 def string_info(a):
-    global calls
-    calls += 1
+    count_calls()
     a = len(a), a.upper(), a.lower()
     print(a)
 
+def is_contains(striing, list_):
+    count_calls()
+    striing = striing.lower()
+    for i in list_:
+        if i.lower() == striing:
+            return True
 
-#def is_contains(s, d):
-
+    return False
 
 string_info('NikiTa')
 string_info('cOpibara')
+
+list_ = ['samMmI', 'saM']
+striing = 'SaM'
+result = is_contains(striing, list_)
+print(result)
+print(calls)
