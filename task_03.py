a11ea3a9ec61efe95b3fc003600d3c022a54e29f
RusNekit <rusnekit@gmail.com>
2024-11-09 19:48:53 +0500

    9/11/2024 tast_03

--- task_03.py
+++ task_03.py
@@ -1,34 +1,17 @@
-def print_params(a=1, b='строка', c=True):
+def print_params(a, b, c):
     print(a, b, c)
 
 
 values_list = [3, 'super', True]
 values_dict = {'a': values_list[0], 'b': values_list[1], 'c': values_list[2]}
 
+values_list_02 = [117, 'Dsuper']
+values_dict_02 = {'c': 44444}
+
 print_params(**values_dict)
+print_params(*values_list_02, **values_dict_02)
 
 
-values_list_2 = ['super', True]
-print_params(*values_list_2, 42)
+# values_list_03 = ['super', True]
+# print_params(*values_list_2, 42)
 
-# Важно!
-#
-# Не передавайте списки задавая по умолчанию пустой список или другой изменяемый тип данных!
-#
-# В таком случае, если этот список будет изменён внутри функции, то на следующий вызов функции он останется в том же состоянии.
-#
-# def a(my_list = [])) – это приводит к ошибкам!
-#
-#
-#
-# Можно передавать вот так(список создаётся локально, мы не влияем на его изменение вне функции)
-#
-# def append_to_list(item, list_my=None):
-#
-#   if list_my is None:
-#
-#    list_my = []
-#
-#   list_my.append(item)
-#
-# print(list_my)
