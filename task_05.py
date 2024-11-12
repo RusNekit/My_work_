68e8cae99dbefa6b55dc6b489cec5a608cf80347
RusNekit <rusnekit@gmail.com>
2024-11-12 19:42:29 +0500

    12/11/2024 tast_05

---
+++ task_05.py
@@ -0,0 +1,13 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) <= 1:
+        return first
+    else:
+        first = int(str_number[0])
+    return first * get_multiplied_digits(int(str_number[1:]))
+
+
+result007 = get_multiplied_digits(233332032)
+print(result007)
+
