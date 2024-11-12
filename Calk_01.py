f34ea27792efb357cee0e8445a5af90824cf1325
RusNekit <rusnekit@gmail.com>
2024-11-12 21:31:58 +0500

    12/11/2024 Calk

---
+++ Calk_01.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Калькулятор')
+window.geometry("350x350")
+window.resizable(False, False)
+button_add = tk.Button(window)
+button_add.place(x=100, y=100)
+window.mainloop()
